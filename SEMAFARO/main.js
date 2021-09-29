135546be68bd3f1db668a7cfa30bcf4b43bf2016
lucasrachid <lucasrachid@hotmail.com>
2021-09-29 20:57:14 -0300

    Atualizando projeto Semafaro

--- SEMAFARO/main.js
+++ SEMAFARO/main.js
@@ -4,23 +4,61 @@ const amarelo = document.querySelector(".amarelo");
 const verde = document.querySelector(".verde");
 const automatico = document.querySelector(".automatico");
 const desativar = document.querySelector(".desativar");
+let colorIndex = 0;
+let intervalId = null;
 
 vermelho.addEventListener("click", () => {
-  semafaro.src = "img/vermelho.png";
+  stopAutomatic();
+  return turnOn.red();
 });
 
 amarelo.addEventListener("click", () => {
-  semafaro.src = "img/amarelo.png";
+  stopAutomatic();
+  return turnOn.yellow();
 });
 
 verde.addEventListener("click", () => {
-  semafaro.src = "img/verde.png";
+  stopAutomatic();
+  return turnOn.green();
 });
 
 desativar.addEventListener("click", () => {
-  semafaro.src = "img/desligado.png";
+  stopAutomatic();
+  return turnOn.turnOff();
 });
 
-// automatico.addEventListener("click", () => {
-//   semafaro.src = "img/vermelho.png";
-// });
+automatico.addEventListener("click", () => {
+  stopAutomatic();
+  return setInterval(() => {
+    changeColor();
+  }, 2000);
+});
+
+const nextIndex = () => {
+  //   if (colorIndex < 3) {
+  //     colorIndex++;
+  //   } else {
+  //     colorIndex = 0;
+  //   }
+  //   //OPERACAO TERNARIA
+  colorIndex = colorIndex < 3 ? ++colorIndex : 0;
+};
+
+const changeColor = () => {
+  const colors = ["red", "yellow", "green", "turnOff"];
+  const color = colors[colorIndex];
+  turnOn[color]();
+  nextIndex();
+};
+
+const turnOn = {
+  red: () => (semafaro.src = "img/vermelho.png"),
+  yellow: () => (semafaro.src = "img/amarelo.png"),
+  green: () => (semafaro.src = "img/verde.png"),
+  automatic: () => (intervalId = setInterval(changeColor, 1000)),
+  turnOff: () => (semafaro.src = "img/desligado.png"),
+};
+
+const stopAutomatic = () => {
+  clearInterval(intervalId);
+};
